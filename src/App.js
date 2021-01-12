5918738128e587e6fc3e442ff622fec99859f1aa
mohamed-ndoye811 <NdoyeMD@outlook.fr>
2021-01-12 14:08:46 +0100

    Panel gauche terminé

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import { gsap, Expo } from "gsap";
 
 // COMPONENTS IMPORT
@@ -8,6 +8,8 @@ import Infos from "./components/Infos";
 import "./App.css";
 
 function App() {
+  const [loadingEnded, setLoadingEnded] = useState(false);
+
   var tl = gsap.timeline();
 
   function logoAnimation() {
@@ -27,21 +29,31 @@ function App() {
     );
   }
 
-  function sliderOut() {
-    tl.to([".slider-bg1", ".slider-bg2", ".slider-bg3"], 1.2, {
-      y: "-100%",
-      stagger: 0.1,
-      ease: Expo.easeInOut,
-    }).to(".sliderLogo", 0.1, { opacity: 0 }, "-=0.6");
-  }
-
   useEffect(() => {
     logoAnimation();
+    function sliderOut() {
+      tl.to([".slider-bg1", ".slider-bg2", ".slider-bg3"], 1.2, {
+        y: "-100%",
+        stagger: 0.1,
+        ease: Expo.easeInOut,
+      }).to(
+        ".sliderLogo",
+        0.1,
+        {
+          opacity: 0,
+          onStart: () => {
+            setLoadingEnded(true);
+          },
+        },
+        "-=0.6"
+      );
+    }
 
-    setTimeout(() => {
+    setInterval(() => {
       sliderOut();
-    }, 3000);
-  });
+    }, 1500);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -55,30 +67,21 @@ function App() {
           className="sliderLogo"
         >
           <g id="logo_N">
-            <polygon
-              points="1945.3,53.2 1724.6,53.2 1506.4,854 1444,854 1444,854 1603.6,268.6 1603.6,268.6 1662.8,53.4 1159.3,53.3 
-                1100.7,266.6 1100.7,266.6 882,1069 1102.6,1069 1320.9,268.6 1320.9,268.6 1382.9,268.6 1223,855.2 1223.1,855 1223.1,855 
-                1164.2,1069.2 1384.9,1069.2 1667.7,1069.2 1726.4,855.6 1726.5,855.6 	"
-            />
+            <polygon points="1945.3,53.2 1724.6,53.2 1506.4,854 1444,854 1444,854 1603.6,268.6 1603.6,268.6 1662.8,53.4 1159.3,53.3 1100.7, 266.6 1100.7, 266.6 882, 1069 1102.6, 1069 1320.9, 268.6 1320.9, 268.6 1382.9, 268.6 1223, 855.2 1223.1, 855 1223.1, 855 1164.2, 1069.2 1384.9, 1069.2 1667.7, 1069.2 1726.4, 855.6 1726.5, 855.6 " />
           </g>
           <g id="logo_M">
-            <polygon
-              points="1101.6,53.4 328.2,53.3 269.5,266.6 269.5,266.6 269,268.5 269,268.6 269,268.6 50.8,1069 271.4,1069 489.7,268.6 
-                489.7,268.6 543.4,268.6 325.3,1069 545.9,1069 764.2,268.6 821.9,268.6 822.3,268.6 604.1,1069 824.7,1069 1043.6,266.6 
-                1043.5,266.6 	"
-            />
+            <polygon points="1101.6,53.4 328.2,53.3 269.5,266.6 269.5,266.6 269,268.5 269,268.6 269,268.6 50.8,1069 271.4,1069 489.7,268.6 489.7, 268.6 543.4, 268.6 325.3, 1069 545.9, 1069 764.2, 268.6 821.9, 268.6 822.3, 268.6 604.1, 1069 824.7, 1069 1043.6, 266.6 1043.5, 266.6" />
           </g>
         </svg>
-        <div className="slider-bg3 slider"></div>
-        <div className="slider-bg2 slider"></div>
-        <div className="slider-bg1 slider"></div>
+        <div className="slider-bg3 slider"> </div>
+        <div className="slider-bg2 slider"> </div>
+        <div className="slider-bg1 slider"> </div>
       </div>
-
       <div className="hub-container">
         <div className="left-container">
-          <Infos />
+          <Infos loadingEnded={loadingEnded} />
         </div>
-        <div className="right-container">rbeb</div>
+        <div className="right-container"> zdz </div>
       </div>
     </>
   );
--- src/components/Infos.js
+++ src/components/Infos.js
@@ -1,18 +1,51 @@
-import React from "react";
+import { React, useEffect } from "react";
+import { gsap, Expo } from "gsap";
 
-export default function Infos() {
+export default function Infos(props) {
+  var tl = gsap.timeline();
+
+  useEffect(() => {
+    if (props.loadingEnded === false) {
+      tl.to([".info-background", ".info"], 1.8, {
+        left: "-100%",
+        ease: Expo.easeInOut,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (props.loadingEnded === true) {
+    tl.to([".info-background", ".info"], 1.8, {
+      left: "0%",
+      ease: Expo.easeInOut,
+    }).from(
+      [".info-picture", ".info-details"],
+      1.8,
+      {
+        opacity: 0,
+        y: 50,
+        ease: Expo.easeInOut,
+        stagger: 0.1,
+      },
+      "-=1.6"
+    );
+  }
+
   return (
-    <div className="info">
-      <div className="info-picture"></div>
-      <div className="info-details">
-        <p className="info-name">MOHAMED NDOYE</p>
-        <p className="info-adress">
-          Bat.76, HTML LA BEAUCAIRE <br /> Place des accacias
-        </p>
-        <p className="info-city">83200 TOULON</p>
-        <p className="info-phone">+33 6 67 07 01 95</p>
-        <p className="info-mail">NdoyeMD@outlook.fr</p>
+    <>
+      <div className="info-background"></div>
+      <div className="info">
+        <div className="info-picture"></div>
+        <div className="info-details">
+          <p className="info-name">MOHAMED NDOYE</p>
+          <p className="info-adress text">
+            Bat.76, HTML LA BEAUCAIRE <br /> Place des accacias
+          </p>
+          <p className="info-city text">83200 TOULON</p>
+          <p className="info-phone text">+33 6 67 07 01 95</p>
+          <p className="info-mail text">NdoyeMD@outlook.fr</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
