5fa15d522054c71231d0242d7aa87ea7251e3f9a
mohamed-ndoye811 <NdoyeMD@outlook.fr>
2021-07-11 00:19:56 +0200

    Experience updatre

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,27 +1,27 @@
-import { React, useEffect } from "react";
-import anime from "animejs";
+import { React, useEffect } from 'react';
+import anime from 'animejs';
 
 //---- Component imports
-import PageTitle from "../components/PageTitle";
+import PageTitle from '../components/PageTitle';
 
 export default function Experience(props) {
   let experienceList = [
     {
       key: 1,
       year: 2021,
-      name: "Stage lsr var",
-      durationLocation: "3 semaines, toulon",
-      tasks: "Création de plugins SketchUp et Revit",
-      link: "https://www.lsr83000.fr/",
-      languageUsed: ["html", "css", "javascript", "svelte"],
+      name: 'Stage lsr var',
+      durationLocation: '3 semaines, toulon',
+      tasks: 'Refonte complète du site internet',
+      link: 'https://www.lsr83000.fr/',
+      languageUsed: ['html', 'css', 'javascript', 'svelte'],
     },
     {
       key: 2,
       year: 2020,
-      name: "Stage Layher",
-      durationLocation: " 2 mois, paris",
-      tasks: "Refonte complète du site internet",
-      languageUsed: ["c_sharp", "ruby"],
+      name: 'Stage Layher',
+      durationLocation: ' 2 mois, paris',
+      tasks: 'Création de plugins SketchUp et Revit',
+      languageUsed: ['c_sharp', 'ruby'],
     },
   ];
 
@@ -29,10 +29,10 @@ export default function Experience(props) {
     let tl = anime.timeline();
 
     tl.add({
-      targets: ".experienceCard",
+      targets: '.experienceCard',
       translateY: [150, 0],
       opacity: [0, 1],
-      easing: "easeOutQuint",
+      easing: 'easeOutQuint',
       duration: duration,
       delay: anime.stagger(125),
     });
@@ -40,19 +40,19 @@ export default function Experience(props) {
 
   useEffect(() => {
     anime({
-      targets: ".container, .pageTitle",
+      targets: '.container, .pageTitle',
       opacity: [0, 0],
     });
 
     if (props.startAnimations) {
       setTimeout(() => {
         anime({
-          targets: ".container",
+          targets: '.container',
           opacity: [0, 1],
         });
 
         anime({
-          targets: ".pageTitle",
+          targets: '.pageTitle',
           opacity: [0, 0.2],
         });
         loadEnterAnimation(1200);
@@ -62,10 +62,10 @@ export default function Experience(props) {
 
   return (
     <>
-      <PageTitle title="Experience" />
-      <main className="experiencePage">
-        <div className="container experienceContainer">
-          <div className="experienceList">
+      <PageTitle title='Experience' />
+      <main className='experiencePage'>
+        <div className='container experienceContainer'>
+          <div className='experienceList'>
             {experienceList
               .sort((a, b) => {
                 return a.year > b.year ? 0 : 1;
@@ -76,29 +76,29 @@ export default function Experience(props) {
                     {experience.link ? (
                       <a
                         href={experience.link}
-                        className="link"
-                        rel="noreferrer"
-                        target="_blank"
+                        className='experienceCard link'
+                        rel='noreferrer'
+                        target='_blank'
                       >
-                        <div className="experienceCard" key={experience.key}>
-                          <div className="year">{experience.year}</div>
-                          <div className="name">{experience.name}</div>
-                          <div className="durationLocation">
-                            {experience.durationLocation}
-                          </div>
-
-                          <div className="tasks">{experience.tasks}</div>
+                        <div className='year'>{experience.year}</div>
+                        <div className='name'>{experience.name}</div>
+                        <div className='durationLocation'>
+                          {experience.durationLocation}
                         </div>
+                        <div className='tasks'>{experience.tasks}</div>
+                        <p className='linkIndication'>
+                          Cliquez pour visiter le site
+                        </p>
                       </a>
                     ) : (
-                      <div className="experienceCard" key={experience.key}>
-                        <div className="year">{experience.year}</div>
-                        <div className="name">{experience.name}</div>
-                        <div className="durationLocation">
+                      <div className='experienceCard'>
+                        <div className='year'>{experience.year}</div>
+                        <div className='name'>{experience.name}</div>
+                        <div className='durationLocation'>
                           {experience.durationLocation}
                         </div>
 
-                        <div className="tasks">{experience.tasks}</div>
+                        <div className='tasks'>{experience.tasks}</div>
                       </div>
                     )}
                   </>
